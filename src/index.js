8fcd84079700e9eda8d73e0ea689f64f04c4ac8a
Jon Q <hello@jonquach.com>
2020-05-18 16:09:18 -0400

    Export FrameContext and FrameContextConsumer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ViewportFrame from './components/ViewportFrame';
+
+export { FrameContext, FrameContextConsumer } from 'react-frame-component';
 export { default as ViewportFrame } from './components/ViewportFrame';
 
 export default ViewportFrame;
